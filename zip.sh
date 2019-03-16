9fc2a4d4a2d4ce8183c1761caa8e7e20f6a70dcd
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2019-03-16 19:18:52 +0100

    Slightly optimized the script to create zipped mod.

--- zip.sh
+++ zip.sh
@@ -9,6 +9,6 @@ mod_name="$(jq -cr '.name' "$info_file")"
 mod_version="$(jq -cr '.version' "$info_file")"
 file_name="${mod_name}_${mod_version}"
 
-cp -R "$source_dir" "./${file_name}"
+cp -Rl "$source_dir" "./${file_name}"
 zip -9 -r "./${file_name}.zip" "./${file_name}"
 rm -rf "./${file_name}"
