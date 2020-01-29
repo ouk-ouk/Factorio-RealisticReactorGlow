8b93d892ca3ea255c3d098cc581b56b431f99547
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2020-01-29 22:15:39 +0100

    Added just a little green to the light so it doesn't look so purple.

--- src/prototypes/reactor-glow-retexture.lua
+++ src/prototypes/reactor-glow-retexture.lua
@@ -13,9 +13,9 @@ nuclearReactorItem.icon = getNewTexturePath(nuclearReactorItem.icon)
 
 local nuclearReactorReactor = data.raw.reactor["nuclear-reactor"]
 if settings.startup[cyanSettingName].value then
-	nuclearReactorReactor.light.color = {b = 0.94, g = 1, r = 0}
+	nuclearReactorReactor.light.color = {b = 0.94, g = 1.0, r = 0.0}
 else
-	nuclearReactorReactor.light.color = {b = 1, g = 0, r = 0}
+	nuclearReactorReactor.light.color = {b = 1.0, g = 0.15, r = 0.0}
 end
 nuclearReactorReactor.working_light_picture.filename = getNewTexturePath(nuclearReactorReactor.working_light_picture.filename)
 nuclearReactorReactor.working_light_picture.hr_version.filename = getNewTexturePath(nuclearReactorReactor.working_light_picture.hr_version.filename)
